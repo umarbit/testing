9187273067b5ad846468994c703e95cf094fe3b7
umarbit <umarbit87@gmail.com>
2018-02-10 18:24:34 -0500

    new message right now

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+package helloworld;
+
+/**
+ * @author Umar
+ */
+public class HelloWorld {
+
+ 
+    //main method declaration
+    public static void main(String[] args) {
+       
+        
+        System.out.print("Hello World umar"); // 'print' will not make a new line
+        System.out.println("This is my output"); //'println' will create a new line after this line of code
+    }
+    
+}
